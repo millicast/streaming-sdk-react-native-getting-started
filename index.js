9b7ed50e8a7fc57d2ca4173974e93cb215bd14a7
Pablo Betancurt <pablo.betancurt@fing.edu.uy>
2022-11-09 17:38:47 -0300

    First commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ window.Logger = MillicastLogger
 
 window.Logger.setLevel(MillicastLogger.DEBUG);
 
-import App from './Viewer';
+import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
