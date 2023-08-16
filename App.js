31e9e4366adc299a0050dfe1e8bd2e4806f8c521
Pablo Betancurt <pablo.betancurt@fing.edu.uy>
2023-08-16 14:40:12 -0300

    Tvapp improved multiview (#59)
    * Made grid for multiview
    
    * Fixed warning
    
    * Feature/grid (#58)
    
    * Re skin
    
    * Styles improved on multiview for TV
    
    * Changed name of buttons in Viewer
    
    * Improved multiview's style
    
    * Improved Publisher's styles
    
    * Fixed 'borderRadius' in iOS
    
    * Disabled headerBar on tvOS
    
    * Updated message
    
    * Removed unncesssary code
    
    * Changed multiview button's name
    
    * Removed unnecessary code
    
    ---------
    
    Co-authored-by: Elisa Montana <108739691+montanaeli@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Elisa Montana <108739691+montanaeli@users.noreply.github.com>

--- App.js
+++ App.js
@@ -7,28 +7,27 @@
  */
 
 import * as React from 'react';
-import { Text, TouchableOpacity, SafeAreaView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {Text, TouchableOpacity, SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import Viewer from './Viewer'
-import Publisher from './Publisher'
+import Viewer from './Viewer';
+import Publisher from './Publisher';
 
-import myStyles from './styles.js'
-import { Platform } from "react-native"
+import myStyles from './styles.js';
+import {Platform} from 'react-native';
 
-function HomeScreen({ navigation }) {
+function HomeScreen({navigation}) {
   return (
     <SafeAreaView style={myStyles.screenContainer}>
       <Text style={myStyles.title}>SAMPLE APP</Text>
-      {!Platform.isTV ?
+      {!Platform.isTV ? (
         <TouchableOpacity
           onPress={() => navigation.navigate('Publisher App')}
           style={myStyles.buttonDesign}>
           <Text style={myStyles.buttonText}>PUBLISHER</Text>
         </TouchableOpacity>
-        : null
-      }
+      ) : null}
 
       <TouchableOpacity
         hasTVPreferredFocus
@@ -44,8 +43,22 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer style={myStyles.screenContainer}>
-      <Stack.Navigator style={myStyles.screenContainer}>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerMode: 'screen',
+          headerTintColor: 'white',
+          headerStyle: {
+            backgroundColor: '#14141A',
+          },
+          headerTitleAlign: 'center',
+          contentStyle: {
+            borderTopColor: '#34343B',
+            borderTopWidth: 1,
+          },
+          // hide the header bar for tvOS due to incompatibility of the library
+          headerShown: !(Platform.OS === 'ios' && Platform.isTV),
+        }}>
         <Stack.Screen name="Millicast SDK Demo" component={HomeScreen} />
         <Stack.Screen name="Subscriber App" component={Viewer} />
         <Stack.Screen name="Publisher App" component={Publisher} />
