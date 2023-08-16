06a7ae221bc190ffbb37e62b71947d6281905b7f
Pablo Betancurt <pablo.betancurt@fing.edu.uy>
2023-08-16 14:39:19 -0300

    Feature/grid (#58)
    * Re skin
    
    * Styles improved on multiview for TV
    
    * Changed name of buttons in Viewer
    
    * Improved multiview's style
    
    * Improved Publisher's styles
    
    * Fixed 'borderRadius' in iOS
    
    * Disabled headerBar on tvOS
    
    * Updated message
    
    * Removed unncesssary code
    
    * Changed multiview button's name
    
    * Removed unnecessary code
    
    ---------
    
    Co-authored-by: Elisa Montana <108739691+montanaeli@users.noreply.github.com>

--- Viewer.js
+++ Viewer.js
@@ -1,26 +1,36 @@
-import { StyleSheet, View, FlatList, Text, TouchableOpacity, SafeAreaView, Dimensions } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  Dimensions,
+  Platform,
+} from 'react-native';
 import React from 'react';
-import { RTCView } from 'react-native-webrtc';
-import { Director, View as MillicastView } from '@millicast/sdk/dist/millicast.debug.umd'
-import myStyles from './styles.js'
+import {RTCView} from 'react-native-webrtc';
+import {
+  Director,
+  View as MillicastView,
+} from '@millicast/sdk/dist/millicast.debug.umd';
+import myStyles from './styles.js';
 
-import { Logger as MillicastLogger } from '@millicast/sdk'
-import { MILLICAST_STREAM_NAME, MILLICAST_ACCOUNT_ID } from '@env';
+import {Logger as MillicastLogger} from '@millicast/sdk';
+import {MILLICAST_STREAM_NAME, MILLICAST_ACCOUNT_ID} from '@env';
 
-const streamName = MILLICAST_STREAM_NAME
-const accountId = MILLICAST_ACCOUNT_ID
+const streamName = MILLICAST_STREAM_NAME;
+const accountId = MILLICAST_ACCOUNT_ID;
 
-const widthScreen = Dimensions.get('screen').width
-const heightScreen = Dimensions.get('screen').height
+const amountCols = Platform.isTV ? 2 : 1;
 
-window.Logger = MillicastLogger
+window.Logger = MillicastLogger;
 
 Logger.setLevel(MillicastLogger.DEBUG);
 
 class MillicastWidget extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       streams: [],
@@ -32,77 +42,82 @@ class MillicastWidget extends React.Component {
       millicastView: null,
       setMedia: true,
       userCount: 0,
-      selectedSource: null
-    }
+      selectedSource: null,
+    };
 
-    this.styles = myStyles
+    this.styles = myStyles;
   }
 
   componentWillUnmount() {
     if (!this.state.setMedia) {
       this.stopStream();
       this.setState({
-        setMedia: true
-      })
+        setMedia: true,
+      });
     }
   }
 
   async subscribe(streamName, accountID) {
-    const tokenGenerator = () => Director.getSubscriber({
-      streamName: streamName,
-      streamAccountId: accountID
-    })
+    const tokenGenerator = () =>
+      Director.getSubscriber({
+        streamName: streamName,
+        streamAccountId: accountID,
+      });
     // Create a new instance
     let view = new MillicastView(streamName, tokenGenerator, null);
     // Set track event handler to receive streams from Publisher.
-    view.on('track', async (event) => {
-      const mediaStream = event.streams[0] ? event.streams[0] : null
-      if (!mediaStream) return null
-      const streams = [...this.state.streams]
+    view.on('track', async event => {
+      const mediaStream = event.streams[0] ? event.streams[0] : null;
+      if (!mediaStream) return null;
+      const streams = [...this.state.streams];
       if (event.track.kind == 'audio') {
-        await Promise.all(streams.map((stream) => {
-          if (stream.stream.toURL() == event.streams[0].toURL()) {
-            stream.audioMid = event.transceiver.mid
-          }
-        }))
+        await Promise.all(
+          streams.map(stream => {
+            if (stream.stream.toURL() == event.streams[0].toURL()) {
+              stream.audioMid = event.transceiver.mid;
+            }
+          }),
+        );
       } else {
-        streams.push({ stream: mediaStream, videoMid: event.transceiver.mid })
+        streams.push({stream: mediaStream, videoMid: event.transceiver.mid});
       }
       this.setState({
-        streams: [...streams]
-      })
-    })
+        streams: [...streams],
+      });
+    });
 
     //Start connection to viewer
     try {
-      view.on("broadcastEvent", async (event) => {
+      view.on('broadcastEvent', async event => {
         //Get event name and data
-        const { name, data } = event;
+        const {name, data} = event;
         switch (name) {
-          case "active":
+          case 'active':
             this.setState({
-              ...((this.state.sourceIds.indexOf(data.sourceId) === -1 && data.sourceId != null) && {
-                sourceIds: [...this.state.sourceIds, data.sourceId],
-              })
-            })
+              ...(this.state.sourceIds.indexOf(data.sourceId) === -1 &&
+                data.sourceId != null && {
+                  sourceIds: [...this.state.sourceIds, data.sourceId],
+                }),
+            });
             //A source has been started on the steam
             break;
-          case "inactive":
+          case 'inactive':
             //A source has been stopped on the steam
             break;
-          case "vad":
+          case 'vad':
             //A new source was multiplexed over the vad tracks
             break;
-          case "layers":
+          case 'layers':
             this.setState({
-              activeLayers: data.medias["0"].active
-            })
+              activeLayers: data.medias['0'].active,
+            });
             //Updated layer information for each simulcast/svc video track
             break;
         }
-
       });
-      await view.connect({ events: ["active", "inactive", "vad", "layers", "viewercount"] })
+      await view.connect({
+        events: ['active', 'inactive', 'vad', 'layers', 'viewercount'],
+      });
 
       // view.on('broadcastEvent', (event) => {
       //   const { name, data } = event
@@ -112,41 +127,48 @@ class MillicastWidget extends React.Component {
       // })
 
       this.setState({
-        millicastView: view
+        millicastView: view,
       });
     } catch (e) {
-      console.error('Connection failed. Reason:', e)
+      console.error('Connection failed. Reason:', e);
     }
   }
 
   stopStream = async () => {
     await this.state.millicastView.stop();
     this.setState();
-  }
+  };
 
-  addRemoteTrack = async (sourceId) => {
-    const mediaStream = new MediaStream()
-    const transceiver = await this.state.millicastView.addRemoteTrack('video', [mediaStream])
+  addRemoteTrack = async sourceId => {
+    const mediaStream = new MediaStream();
+    const transceiver = await this.state.millicastView.addRemoteTrack('video', [
+      mediaStream,
+    ]);
     const mediaId = transceiver.mid;
-    await this.state.millicastView.project(sourceId, [{
-      media: 'video',
-      mediaId,
-      trackId: 'video'
-    }])
+    await this.state.millicastView.project(sourceId, [
+      {
+        media: 'video',
+        mediaId,
+        trackId: 'video',
+      },
+    ]);
     this.setState({
-      streams: [...this.state.streams, { stream: mediaStream, videoMid: mediaId }],
-    })
-  }
+      streams: [
+        ...this.state.streams,
+        {stream: mediaStream, videoMid: mediaId},
+      ],
+    });
+  };
 
   changeStateOfMediaTracks(streams, value) {
-    streams.map(s => (
+    streams.map(s =>
       s.stream.getTracks().forEach(videoTrack => {
         videoTrack.enabled = value;
-      })
-    ))
+      }),
+    );
     this.setState({
-      playing: value
-    })
+      playing: value,
+    });
   }
 
   playPauseVideo = async () => {
@@ -155,141 +177,193 @@ class MillicastWidget extends React.Component {
 
       this.subscribe(streamName, accountId);
       this.setState({
-        setMedia: false
-      })
+        setMedia: false,
+      });
     }
     let isPaused = !this.state.playing;
-    if (isPaused) {
-      this.changeStateOfMediaTracks(this.state.streams, isPaused);
-    } else {
-      this.changeStateOfMediaTracks(this.state.streams, isPaused)
-    }
-  }
+    this.changeStateOfMediaTracks(this.state.streams, isPaused);
+  };
 
   changeStateOfAudioTracks(streams, value) {
-    streams.map(s => (
+    streams.map(s =>
       s.stream.getTracks().forEach(track => {
-        if (track.kind == "audio") {
+        if (track.kind == 'audio') {
           track.enabled = value;
         }
-      })
-    ))
+      }),
+    );
     this.setState({
-      muted: !value
-    })
+      muted: !value,
+    });
   }
 
   muteAudio = async () => {
     let isPaused = this.state.muted;
-    if (!isPaused) {
-      this.changeStateOfAudioTracks(this.state.streams, isPaused);
-    } else {
-      this.changeStateOfAudioTracks(this.state.streams, isPaused);
-    }
+    this.changeStateOfAudioTracks(this.state.streams, isPaused);
     isPaused = !isPaused;
-  }
+  };
 
   project = async (sourceId, videoMid, audioMid) => {
     if (sourceId == 'main') {
-      sourceId = null
+      sourceId = null;
     }
-    this.state.millicastView.project(sourceId, [{
-      media: 'video',
-      mediaId: videoMid,
-      trackId: 'video'
-    }])
+    this.state.millicastView.project(sourceId, [
+      {
+        media: 'video',
+        mediaId: videoMid,
+        trackId: 'video',
+      },
+    ]);
     if (audioMid) {
-      this.state.millicastView.project(sourceId, [{
-        media: 'audio',
-        mediaId: this.state.streams[0].audioMid,
-        trackId: 'audio'
-      }])
+      this.state.millicastView.project(sourceId, [
+        {
+          media: 'audio',
+          mediaId: this.state.streams[0].audioMid,
+          trackId: 'audio',
+        },
+      ]);
     }
-  }
+  };
 
-  select = async (id) => {
-    this.state.millicastView.select({ encodingId: id })
-  }
+  select = async id => {
+    this.state.millicastView.select({encodingId: id});
+  };
 
   multiView = async () => {
-    const sourceIds = this.state.sourceIds
+    const sourceIds = this.state.sourceIds;
     if (!this.state.multiView) {
-      await Promise.all(sourceIds.map(async (sourceId) => {
-        if (sourceId != 'main') {
-          this.addRemoteTrack(sourceId)
-        }
-      }))
-      this.setState({ streams: [this.state.streams[0]] })
+      await Promise.all(
+        sourceIds.map(async sourceId => {
+          if (sourceId != 'main') {
+            this.addRemoteTrack(sourceId);
+          }
+        }),
+      );
+      this.setState({streams: [this.state.streams[0]]});
     } else {
       this.setState({
-        streams: [...[this.state.streams[0]]]
-      })
+        streams: [...[this.state.streams[0]]],
+      });
     }
     this.setState({
-      multiView: !this.state.multiView
-    })
-  }
+      multiView: !this.state.multiView,
+    });
+  };
 
   render() {
     return (
       <>
-        {
-          this.state.multiView ?
-            // multiview
+        {this.state.multiView ? (
+          // multiview
+          <View
+            style={{
+              alignContent: 'center',
+              marginBottom: 50,
+            }}>
             <FlatList
               data={this.state.streams}
               style={{
-                textAlign: 'center'
+                textAlign: 'center',
               }}
-              numColumns={2}
+              numColumns={amountCols}
               keyExtractor={(_, index) => String(index)}
-              renderItem={({ item, index }) => (
-                <View>
-                  <TouchableOpacity
-                    hasTVPreferredFocus
-                    onPress={() => {
-                      this.setState({ selectedSource: item.stream.toURL() })
-                      this.setState({ multiView: !this.state.multiView })
+              renderItem={({item, index}) => (
+                <View
+                  style={
+                    amountCols === 2
+                      ? [
+                          {marginTop: -90, marginBottom: -100},
+                          index % 2 == 0
+                            ? {marginLeft: 10, marginRight: 5}
+                            : {marginLeft: 5, marginRight: 10},
+                        ]
+                      : [
+                          {
+                            marginTop: -75,
+                            marginBottom: -85,
+                            marginLeft: '2.5%',
+                            marginRight: '2.5%',
+                          },
+                        ]
+                  }>
+                  <View
+                    style={{
+                      position: 'absolute',
+                      left: 8,
+                      bottom: 108,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      zIndex: 1,
+                      padding: 5,
+                      borderRadius: 2,
+                      backgroundColor: 'rgba(0,0,0,.288)',
                     }}>
-                    <Text style={{ color: 'black' }}>
-                      {item.stream.videoMid === '0' ? 'Main' : String(this.state.sourceIds[index])}
-                    </Text>
-                  </TouchableOpacity>
+                    <TouchableOpacity
+                      hasTVPreferredFocus
+                      onPress={() => {
+                        this.setState({selectedSource: item.stream.toURL()});
+                        this.setState({multiView: !this.state.multiView});
+                      }}>
+                      <Text style={{color: 'white'}}>
+                        {item.stream.videoMid === '0'
+                          ? 'Main'
+                          : String(this.state.sourceIds[index])}
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
                   <RTCView
                     key={item.stream.toURL() + item.stream.videoMid}
                     streamURL={item.stream.toURL()}
-                    style={{ width: widthScreen * 0.45, height: heightScreen * 0.4 }}
+                    style={{
+                      width: amountCols === 2 ? '70%' : '100%',
+                      flex: 1,
+                      aspectRatio: 1,
+                      borderRadius: 30,
+                    }}
                   />
                 </View>
               )}
             />
-            :
-            // main/selected source
-            this.state.streams[0]
-              ?
-              <RTCView
-                key={this.state.selectedSource ?? 'main'}
-                streamURL={this.state.selectedSource ?? this.state.streams[0].stream.toURL()}
-                style={this.styles.video}
-                objectFit='contain' />
-              :
-              <Text style={{ color: 'black' }}>Press the 'play' button, or the 'multiview' button.</Text>
-        }
-        {<View style={myStyles.bottomMultimediaContainer}>
-          <View style={myStyles.bottomIconWrapper}>
-            <TouchableOpacity
-              hasTVPreferredFocus
-              tvParallaxProperties={{ magnification: 1.2 }}
-              onPress={this.playPauseVideo} >
-              <Text style={{ color: 'black' }}>Play</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={this.multiView} >
-              <Text style={{ color: 'black' }}>Multiview</Text>
-            </TouchableOpacity>
           </View>
-        </View>
+        ) : // main/selected source
+        this.state.streams[0] ? (
+          <RTCView
+            key={this.state.selectedSource ?? 'main'}
+            streamURL={
+              this.state.selectedSource ?? this.state.streams[0].stream.toURL()
+            }
+            style={this.styles.video}
+            objectFit="contain"
+          />
+        ) : (
+          <View style={{padding: '5%'}}>
+            <Text style={{color: 'white'}}>
+              Press the 'play' button to start watching.
+            </Text>
+          </View>
+        )}
+        {
+          <View style={myStyles.bottomMultimediaContainer}>
+            <View style={myStyles.bottomIconWrapper}>
+              <TouchableOpacity
+                hasTVPreferredFocus
+                tvParallaxProperties={{magnification: 1.2}}
+                onPress={this.playPauseVideo}>
+                <Text style={{color: 'white', fontWeight: 'bold'}}>
+                  {this.state.playing ? 'Pause' : 'Play'}
+                </Text>
+              </TouchableOpacity>
+              {this.state.playing ? (
+                <TouchableOpacity onPress={this.multiView}>
+                  <Text style={{color: 'white', fontWeight: 'bold'}}>
+                    {this.state.multiView ? 'Go back' : 'Multiview'}
+                  </Text>
+                </TouchableOpacity>
+              ) : null}
+            </View>
+          </View>
 
-/* 
+          /* 
         <View style={myStyles.topViewerCount}>
           <Ionicons name="ios-person" size={30} color="#7f00b2" />
           <Text style={{ fontWeight: 'bold' }}>{`${this.state.userCount}`}</Text>
@@ -313,20 +387,18 @@ class MillicastWidget extends React.Component {
             })
             }
           </View>
-        </View> */}
+        </View> */
+        }
       </>
     );
   }
-};
+}
 
 export default function App() {
   return (
     <>
       <SafeAreaView style={stylesContainer.container}>
-        <MillicastWidget
-          streamName={streamName}
-          accountID={accountId}
-        />
+        <MillicastWidget streamName={streamName} accountID={accountId} />
       </SafeAreaView>
     </>
   );
@@ -334,7 +406,7 @@ export default function App() {
 
 const stylesContainer = StyleSheet.create({
   container: {
-    // backgroundColor: Colors.white,
-    ...StyleSheet.absoluteFill
+    backgroundColor: '#14141A',
+    ...StyleSheet.absoluteFill,
   },
 });
