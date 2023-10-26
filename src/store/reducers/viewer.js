aec3ee08b5c3329f8157e91ee810189db37de08b
Santiago Souto <ssout@dolby.com>
2023-10-26 14:30:54 -0300

    Add state reducers

--- src/store/reducers/viewer.js
+++ src/store/reducers/viewer.js
@@ -8,6 +8,8 @@ const initialState = {
   millicastView: null,
   setMedia: true,
   selectedSource: null,
+  muted: false,
+  multiView: false
 };
 
 const viewerReducer = (state = initialState, action) => {
@@ -27,6 +29,33 @@ const viewerReducer = (state = initialState, action) => {
         ...state,
         streams: action.payload,
       };
+    case 'viewer/setSourceIds':
+      return {
+        ...state,
+        sourceIds: action.payload
+      };
+    case 'viewer/setActiveLayers':
+      return {
+        ...state,
+        activeLayers: action.payload
+      };
+    case 'viewer/setPlaying':
+      return {
+        ...state,
+        playing: action.payload
+      };
+    case 'viewer/setMuted':
+      return {
+        ...state,
+        muted: action.payload
+      }
+    case 'viewer/setMultiview':
+      return {
+        ...state,
+        multiView: action.payload
+      };
+    case 'viewer/stopStream':
+      return initialState;
     default:
       return state;
   }
