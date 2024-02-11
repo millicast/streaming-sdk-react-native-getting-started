c294df20086c3fab396d702fe71a1da43f220731
Tarun Garg <112677108+tarunkgarg@users.noreply.github.com>
2024-02-11 18:25:56 +1100

    Applied app style wrapper component to other component styling object

--- src/components/BottomBar/BottomBar.tsx
+++ src/components/BottomBar/BottomBar.tsx
@@ -1,18 +1,20 @@
 import { Icon } from '@dolbyio/uikit-react-native';
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, Platform } from 'react-native';
 
 import styles from './BottomBar.style';
 
 export const BottomBar = ({ displayStatsInformation }) => {
+  const iconSize = Platform.OS === 'android' && Platform.isTV ? 's' : 'm';
   return (
     <View style={styles.wrapper}>
       <TouchableOpacity
+        hasTVPreferredFocus
         onPress={() => {
           displayStatsInformation();
         }}
       >
-        <Icon testID="infoIcon" name="info" size="m" />
+        <Icon testID="infoIcon" name="info" size={iconSize} />
       </TouchableOpacity>
     </View>
   );
