00122b54d71e8c34eaa63c67544aafaa45e4a8e8
Tarun Garg <112677108+tarunkgarg@users.noreply.github.com>
2024-02-20 11:47:26 +1100

    Made a fix to handle fire tv input

--- src/components/FocusedComponent/FocusedComponent.tsx
+++ src/components/FocusedComponent/FocusedComponent.tsx
@@ -27,14 +27,39 @@ export const FocusedComponent = ({
   const [isFocused, setIsFocused] = useState(false);
   const { style, underlayColor, ...restProps } = props;
 
+  const getDeviceMake = (): string => {
+    let make = 'mobile';
+    if (Platform.isTV) {
+      if (Platform.OS === 'ios') {
+        make = 'appletv';
+      } else {
+        // for android tv make is either Google or Amazon
+        make = Platform.constants.Manufacturer?? 'unknown';
+      }
+    }
+    return make.toLowerCase();
+  };
+
+  const deviceMake = getDeviceMake();
+  const borderNormalColor = '#E0E0E5';
+  const borderHighlightColor = '#AA33FF';
+  const [borderColor, setBorderColor] = useState<string>(borderNormalColor);
+
   // console.debug(`${props?.testID ?? 'no name'} props :: `, JSON.stringify(props ?? {}));
 
   const onPress = () => {
-    // This method is triggered only for tvOS device
-    if (Platform.isTV && Platform.OS === 'ios' && componentRef?.current) {
+    // This method is triggered tvOS and fire tv device
+
+    // list of devices - which opens keyboard on press event
+    const deviceList = ['amazon', 'appletv'];
+
+    setIsFocused(true);
+
+    if (Platform.isTV && deviceList.includes(deviceMake) && componentRef?.current) {
+      // set focus for apple and fire tv only
       componentRef?.current.focus();
-      setIsFocused(true);
     }
+
     // invoke this callback to trigger style change in parent component
     if (setParentFocus) {
       setParentFocus(true);
@@ -43,10 +68,22 @@ export const FocusedComponent = ({
 
   const onFocus = () => {
     // This method is triggered only for android tv device
+
+    // list of devices - which needs border color change
+    const deviceList = ['amazon', 'appletv'];
     setIsFocused(true);
-    if (Platform.isTV && Platform.OS === 'android' && componentRef?.current) {
+
+    if (Platform.isTV && deviceList.includes(deviceMake)) {
+      // set border color for apple and fire tv only
+      setBorderColor(borderHighlightColor);
+    }
+
+    if (Platform.isTV && Platform.OS === 'android' && !deviceList.includes(deviceMake) && componentRef?.current) {
+      // for android tv keyboard is displayed on onfocus event only
+      // set input focus for android tv only
       componentRef?.current.focus();
     }
+
     // invoke this callback to trigger style change in parent component
     if (setParentFocus) {
       setParentFocus(true);
@@ -55,7 +92,17 @@ export const FocusedComponent = ({
 
   const onBlur = () => {
     // This method is triggered for android & apple tv device
+
+    // list of devices - which needs border color change
+    const deviceList = ['amazon', 'appletv'];
+
+    if (Platform.isTV && deviceList.includes(deviceMake)) {
+      // reset border color for apple and fire tv only
+      setBorderColor(borderNormalColor);
+    }
+
     setIsFocused(false);
+
     if (setParentFocus) {
       setParentFocus(false);
     }
@@ -70,7 +117,7 @@ export const FocusedComponent = ({
       underlayColor={underlayColor ?? 'none'}
       {...restProps}
     >
-      {children}
+      {React.cloneElement(children, { borderColor })}
     </TouchableHighlight>
   ) : (
     <Pressable {...props}>{children}</Pressable>
