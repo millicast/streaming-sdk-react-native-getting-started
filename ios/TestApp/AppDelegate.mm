7c641c0d4a1d9c40e2c5484d37d539f28f773adc
Aravind Raveendran <aravind.raveendran@dolby.com>
2024-04-23 16:45:23 +1000

    Consume suppressing microphone permissions fix in react-native-webrtc

--- ios/TestApp/AppDelegate.mm
+++ ios/TestApp/AppDelegate.mm
@@ -5,6 +5,7 @@
 #import <React/RCTRootView.h>
 
 #import <React/RCTAppSetupUtils.h>
+#import <react-native-webrtc/WebRTCModuleOptions.h>
 
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
